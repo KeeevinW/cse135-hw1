b6200f8ffe89925f559bf11410c5c990df84f2a0
KeeevinW <xuanye_wang@ucsb.edu>
2026-01-31 11:39:34 -0800

    add python hello world cgi

---
+++ cgi-bin/python-hello-html-world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import datetime
+
+print("Content-Type: text/html\n\n")
+print("<html><head><title>Hello Python</title></head><body>")
+print("<h1>Hello Team Xuanye (Python)</h1>")
+print("<p>This page was generated with Python 3.</p>")
+print(f"<p>Current Time: {datetime.datetime.now()}</p>")
+print("</body></html>")
