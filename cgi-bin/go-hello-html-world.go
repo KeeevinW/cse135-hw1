c12dacd6eb50390b70a757a77eebc2f2f9dee36f
KeeevinW <xuanye_wang@ucsb.edu>
2026-02-03 02:06:22 -0800

    update go hellp

--- cgi-bin/go-hello-html-world.go
+++ cgi-bin/go-hello-html-world.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
+	userIP := os.Getenv("REMOTE_ADDR")
+	if userIP == "" {
+		userIP = "Unknown"
+	}
 	fmt.Print("Content-Type: text/html\n\n")
 	fmt.Print("<html><head><title>Hello Go</title></head><body>")
 	fmt.Print("<h1>Greetings from Xuanye!</h1>")
 	fmt.Print("<p>This page was generated with Go (Compiled).</p>")
+	fmt.Printf("<p>Your IP Address: <strong>%s</strong></p>", userIP)
 	fmt.Printf("<p>Current Time: %s</p>", time.Now().Format(time.RFC1123))
 	fmt.Print("</body></html>")
 }
--- cgi-bin/go-hello-json-world.go
+++ cgi-bin/go-hello-json-world.go
@@ -3,16 +3,23 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	fmt.Print("Content-Type: application/json\n\n")
 
+	userIP := os.Getenv("REMOTE_ADDR")
+	if userIP == "" {
+		userIP = "Unknown"
+	}
+
 	data := map[string]string{
-		"message":  " Greetings from Xuanye!",
-		"language": "Go",
-		"time":     time.Now().String(),
+		"message":    "Greetings from Xuanye!",
+		"language":   "Go",
+		"ip_address": userIP,
+		"time":       time.Now().Format(time.RFC3339),
 	}
 
 	jsonOutput, _ := json.Marshal(data)
