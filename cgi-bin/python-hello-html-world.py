d887861607feba85be80bc9fedcd0ce52cac168a
KeeevinW <xuanye_wang@ucsb.edu>
2026-02-02 21:45:42 -0800

    add ip address in python html hellow world

--- cgi-bin/python-hello-html-world.py
+++ cgi-bin/python-hello-html-world.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import datetime
+import os
+ip_address = os.environ.get('REMOTE_ADDR', '127.0.0.1')
 
 print("Content-Type: text/html\n\n")
 print("<html><head><title>Hello Python</title></head><body>")
 print("<h1>Hello Team Xuanye (Python)</h1>")
 print("<p>This page was generated with Python 3.</p>")
 print(f"<p>Current Time: {datetime.datetime.now()}</p>")
+print(f"<p>Your IP Address: <strong>{ip_address}</strong></p>")
 print("</body></html>")
