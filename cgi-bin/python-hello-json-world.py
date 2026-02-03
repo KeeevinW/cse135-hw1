f50b97035b5fc810eae40c3cb697d868169d10b9
KeeevinW <xuanye_wang@ucsb.edu>
2026-02-02 21:47:20 -0800

    add greetings

--- cgi-bin/python-hello-json-world.py
+++ cgi-bin/python-hello-json-world.py
@@ -6,7 +6,7 @@ import os
 print("Content-Type: application/json\n\n")
 
 data = {
-    "message": "Hello Team Xuanye",
+    "message": "Greetings from Xuanye!",
     "language": "Python 3",
     "date": str(datetime.datetime.now()),
     "ip": os.environ.get("REMOTE_ADDR", "Unknown")
