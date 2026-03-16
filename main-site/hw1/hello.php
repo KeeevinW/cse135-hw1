d68ace8d9f9722fa2639fffa1c2e8b7ead2b97f3
KeeevinW <xuanye_wang@ucsb.edu>
2026-03-15 23:44:08 -0700

    update extra credit

--- main-site/hw1/hello.php
+++ main-site/hw1/hello.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Hello PHP</title>
+    <script src="https://collector.xuanye.site/collector.js"></script>
 </head>
 <body>
     <h1>PHP Lives!</h1>
